2b97525e806cca5fd70f3d0e639bba734a93b785
Alex Gur <alexdream81@gmail.com>
2021-08-02 13:05:13 +0300

    fix new OneMoment

--- workflow/custom-moment-js/app.js
+++ workflow/custom-moment-js/app.js
@@ -1,41 +1,52 @@
-const nowDate = new Date();
-const customDate = new Date(2012, 13, 21);
+function OneMoment(date) {
+    this.date = date;
+    this.format = function () {
+        let fullDate = new Date(date);
+        let month = '' + (fullDate.getMonth() + 1);
+        let year = fullDate.getFullYear();
 
-function formatAnotherDate(date) {
-    let fullDate = new Date(date);
-    let month = '' + (fullDate.getMonth() + 1);
-    let year = fullDate.getFullYear();
-
-    if (month.length < 2) {
-        month = '0' + month;
-    }
-    return [month, year].join('-');
+        if (month.length < 2) {
+            month = '0' + month;
+        }
+        return [month, year].join('-');
+    };
+    this.fromNow = function () {
+        const differenceInTime = now.getTime() - custom.getTime();
+        let years = Math.ceil(differenceInTime / (3600 * 24 * 1000 * 365));
+        let days = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+        let result = '';
+        days < 365
+            ? (result = `in ${days} days`)
+            : (result = `${years} years ago`);
+        return result;
+    };
+    this.toDate = function () {
+        return this.date instanceof Date;
+    };
 }
 
-function formatDate(date) {
-    return date.toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-    });
+const now = new OneMoment(new Date());
+
+const someDate = new OneMoment(Date.parse(reverseDate('21-12-2012')));
+const anotherDate = new OneMoment(Date.parse(formatDate('01202019')));
+
+console.log(now);
+console.log(someDate);
+console.log(anotherDate);
+
+function reverseDate(string) {
+    let reverseString = string.split('-').reverse().join('-');
+    return reverseString;
 }
 
-console.log(formatDate(nowDate));
-console.log(formatAnotherDate(nowDate));
-
-function getDifferenceDate(custom, now) {
-    const differenceInTime = now.getTime() - custom.getTime();
-    let years = Math.ceil(differenceInTime / (3600 * 24 * 1000 * 365));
-    let days = Math.ceil(differenceInTime / (1000 * 3600 * 24));
-    let result = '';
-    days < 365 ? (result = `in ${days} days`) : (result = `${years} years ago`);
-    return result;
+function formatDate(str) {
+    let array = str.split('');
+    let newArray = [];
+    let year = array.slice(4, 8).join('');
+    newArray.push(year);
+    let month = array.slice(0, 2).join('');
+    newArray.push(month);
+    let day = array.slice(2, 4).join('');
+    newArray.push(day);
+    return newArray.join('-');
 }
-
-console.log(getDifferenceDate(customDate, nowDate));
-
-function checkInstance(date) {
-    return date instanceof Date;
-}
-
-console.log(checkInstance(nowDate));
