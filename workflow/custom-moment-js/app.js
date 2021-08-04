713276f36ef546a72cd82c9cbe633d19cbf67c1b
Alex Gur <alexdream81@gmail.com>
2021-08-04 16:07:12 +0300

    fix: correct my mistakes in PR

--- workflow/custom-moment-js/app.js
+++ workflow/custom-moment-js/app.js
@@ -1,13 +1,24 @@
 // const now = new OneMoment(12323323);
 const now = new OneMoment(new Date());
+const someDate = OneMoment.parse('21-12-2012', 'DD-MM-YYYY');
+const anotherDate = OneMoment.parse('01202019', 'MMDDYYYY');
+const date = someDate.toDate();
+
+console.log(someDate);
+console.log(anotherDate);
+console.log(someDate.format('YYYY/MM/DD'));
+console.log(anotherDate.format('MM-YYYY'));
+console.log(someDate.fromNow());
+console.log(anotherDate.fromNow());
+console.log(date instanceof Date);
 
 function OneMoment(date = new Date(), formatString) {
     if (typeof date === 'number') {
         this.msDate = new Date(date);
     }
     this.date = date;
+    this.fullDate = new Date(this.date);
     this.formatStringDate = formatString;
-    this.nowDate = new Date();
 
     OneMoment.parse = function (formatDate, formatString) {
         this.date = formatDate;
@@ -31,35 +42,32 @@ function OneMoment(date = new Date(), formatString) {
     };
 
     this.format = function (string) {
-        let fullDate = new Date(this.date);
-        if (string == 'MM-YYYY') {
-            let month = '' + (fullDate.getMonth() + 1);
-            let year = fullDate.getFullYear();
-
-            if (month.length < 2) {
+        let year = '';
+        let month = '';
+        switch (string) {
+            case 'MM-YYYY':
+                month = '' + (this.fullDate.getMonth() + 1);
+                year = this.fullDate.getFullYear();
+            case 'month.length < 2':
                 month = '0' + month;
-            }
-            return [month, year].join('-');
-        } else {
-            const result = fullDate.toLocaleDateString('en-US', {
-                month: '2-digit',
-                day: '2-digit',
-                year: 'numeric',
-            });
-            let reverseDate = result.split('');
-            let newArray = [];
-            let year = reverseDate.slice(6, 10).join('');
-            newArray.push(year);
-            let month = reverseDate.slice(0, 2).join('');
-            newArray.push(month);
-            let day = reverseDate.slice(3, 5).join('');
-            newArray.push(day);
-            return newArray.join('/');
+                return `${month}-${year}`;
+            case 'YYYY/MM/DD':
+                const result = this.fullDate.toLocaleDateString('en-US', {
+                    month: '2-digit',
+                    day: '2-digit',
+                    year: 'numeric',
+                });
+                let newFullDate = new Date(result);
+                year = newFullDate.getFullYear();
+                month = newFullDate.getMonth();
+                let day = newFullDate.getDate();
+                return `${year}/${month + 1}/${day}`;
         }
     };
+
     this.fromNow = function () {
         const customDate = new Date(this.date);
-        const now = new Date(this.nowDate);
+        const now = new Date();
         const differenceInTime = now.getTime() - customDate.getTime();
         const years = Math.ceil(differenceInTime / (3600 * 24 * 1000 * 365));
         const days = Math.ceil(differenceInTime / (1000 * 3600 * 24));
@@ -69,13 +77,9 @@ function OneMoment(date = new Date(), formatString) {
             : (result = `${years} years ago`);
         return result;
     };
+
     this.toDate = function () {
         const dateData = new Date(someDate);
         return dateData;
     };
 }
-
-const someDate = OneMoment.parse('21-12-2012', 'DD-MM-YYYY');
-const anotherDate = OneMoment.parse('01202019', 'MMDDYYYY');
-console.log(someDate);
-console.log(anotherDate);
