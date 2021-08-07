9c884c9be7c508b460a207c2babe7ba28c16911e
Alex Gur <alexdream81@gmail.com>
2021-08-07 23:14:44 +0300

    feat: add mathjs libraries

--- workflow/function-cacher/main.js
+++ workflow/function-cacher/main.js
@@ -2,10 +2,36 @@ const value = document.getElementById('input');
 const calculateButton = document.getElementById('calculate-btn');
 const outBlock = document.getElementById('out-block');
 
-console.log(value, calculateButton, outBlock);
-class Cacher {
-    constructor() {}
-    withCache() {}
-}
+// factorial = cacher.withCache(factorial);
+// const a = factorial(someBigNumber);
+// const b = factorial(someBigNumber);
 
-const cacher = new Cacher();
+calculateButton.addEventListener('click', () => {
+    let number = value.value;
+    outBlock.innerHTML = getFactorial(number);
+});
+
+const getFactorial = (n) => {
+    let factorial = math.factorial(n);
+    return factorial;
+};
+
+// class Cacher {
+//     constructor() {
+//         this.cache = {};
+//     }
+//     withCache(...args) {
+//         let n = args[0];
+//         if (n in cache) {
+//             console.log('Fetching from cache', n);
+//             return cache[n];
+//         } else {
+//             console.log('Calculating result', n);
+//             let result = fn(n);
+//             cache[n] = result;
+//             return result;
+//         }
+//     }
+// }
+
+// const cacher = new Cacher();
