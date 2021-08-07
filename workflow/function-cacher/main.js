9ab54024fe81ab7699308a28ab2dc132b8610c7e
Alex Gur <alexdream81@gmail.com>
2021-08-07 08:56:52 +0300

    init task

---
+++ workflow/function-cacher/main.js
@@ -0,0 +1,11 @@
+const value = document.getElementById('input');
+const calculateButton = document.getElementById('calculate-btn');
+const outBlock = document.getElementById('out-block');
+
+console.log(value, calculateButton, outBlock);
+class Cacher {
+    constructor() {}
+    withCache() {}
+}
+
+const cacher = new Cacher();
