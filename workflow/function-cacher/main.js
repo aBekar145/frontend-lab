8c12496b3bffd26698014293917e6f2da32bcd7e
Alex Gur <alexdream81@gmail.com>
2021-08-10 11:30:13 +0300

    fix: correct cachedFactorial function and change variables

--- workflow/function-cacher/main.js
+++ workflow/function-cacher/main.js
@@ -1,31 +1,19 @@
-const value = document.getElementById('input');
+const valueInput = document.getElementById('input');
 const calculateButton = document.getElementById('calculate-btn');
-const outBlock = document.getElementById('out-block');
-let someBigNumber = 0;
-
-calculateButton.addEventListener('click', () => {
-    someBigNumber = value.value;
-    cachedFactorial = cacher.withCache(factorial);
-    const a = cachedFactorial(someBigNumber);
-    outBlock.innerHTML = a;
-});
-
-const factorial = (n) => {
-    let factorial = math.factorial(n);
-    return factorial;
-};
+const outResultBlock = document.getElementById('out-block');
 
 class Cacher {
     constructor() {}
-    withCache(fn) {
-        let cache = {};
+    withCache(calculatedFactorial) {
+        const cache = {};
         return (...args) => {
-            let n = args[0];
-            if (n in cache) {
-                return cache[n];
+            const number = args[0];
+            if (number in cache) {
+                return cache[number];
             } else {
-                let result = fn(n);
-                cache[n] = result;
+                const result = calculatedFactorial(number);
+                cache[number] = result;
+                console.log(cache);
                 return result;
             }
         };
@@ -33,3 +21,15 @@ class Cacher {
 }
 
 const cacher = new Cacher();
+
+const factorial = (number) => {
+    const factorial = math.factorial(number);
+    return factorial;
+};
+
+const cachedFactorial = cacher.withCache(factorial);
+
+calculateButton.addEventListener('click', () => {
+    const result = cachedFactorial(valueInput.value);
+    outResultBlock.innerHTML = result;
+});
