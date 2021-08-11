52ac54db5845d87b9132c683b240c30a486a81bb
Alex Gur <alexdream81@gmail.com>
2021-08-11 18:16:20 +0300

    feat: add tabs block

--- workflow/tabs/main.js
+++ workflow/tabs/main.js
@@ -1,11 +1,22 @@
 const tabsButton = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
 
 tabsButton.forEach((item) => {
     item.addEventListener('click', () => {
-        let currentButton = item;
-        tabsButton.forEach((item) => {
-            item.classList.remove('active');
-        });
-        currentButton.classList.add('active');
+        const currentButton = item;
+        const tabId = currentButton.getAttribute('data-tab');
+        const currentTab = document.querySelector(tabId);
+        if (!currentButton.classList.contains('active')) {
+            tabsButton.forEach((item) => {
+                item.classList.remove('active');
+            });
+            tabsItems.forEach((item) => {
+                item.classList.remove('active');
+            });
+            currentButton.classList.add('active');
+            currentTab.classList.add('active');
+        }
     });
 });
+
+document.querySelector('.tabs__nav-btn').click();
