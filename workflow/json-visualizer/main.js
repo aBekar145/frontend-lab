78a1c76a6545eb97acb448ba9276b90532eafa1c
Alex Gur <alexdream81@gmail.com>
2021-08-16 11:38:05 +0300

    feat: add click on button

--- workflow/json-visualizer/main.js
+++ workflow/json-visualizer/main.js
@@ -3,40 +3,78 @@ const fieldOutResult = document.getElementById('show-here');
 const button = document.getElementById('text-area-button');
 
 const valueInput =
-    (fieldInput.value = `{\n  "key":"value",\n  "key2":2,\n  "key3":["value","value", { "key1": 1, "key2": 2 }],\n  "key4":{"key":2}\n,  key: 3},`);
+    (fieldInput.value = `{\n  "key":"value",\n  "key2":2,\n  "key3":["value","value", { "key1": 1, "key2": 2 }],\n  "key4":{"key":2}\n}`);
 
-function jsonToHTML(input) {
-    return (show) => {
-        return () => {
-            const json = parseInput(input);
-            const display = show ? 'block' : 'none';
-            const repSm = /</gi;
-            const repGt = />/gi;
-            const htmlArray = [`ul style="display: ${display}">`];
-            for (let [key, value] of Object.entries(json)) {
-                if (
-                    typeof value === 'object' &&
-                    Object.keys(value).length > 0
-                ) {
-                    htmlArray.push(
-                        `<li>${key}:<span class="clickable" style="cursor: pointer">+</span>`
-                    );
-                }
+const jsonToHtml = (data) => {
+    console.log(data);
+    const json = parseInput(data);
+    const repSm = /</gi;
+    const repGt = />/gi;
+    const htmlArray = [`<ul style="display: block">`];
+    for (let [key, value] of Object.entries(json)) {
+        if (typeof value === 'object' && Object.keys(value).length > 0) {
+            htmlArray.push(
+                `<li>${key}:<span class="clickable" style="cursor: pointer">+</span>`
+            );
+        } else {
+            let content;
+            if (Array.isArray(value)) {
+                content = '[]';
+            } else if (typeof value === 'object') {
+                content = '{}';
+            } else {
+                const formattedContent =
+                    typeof value === 'string'
+                        ? value.replace(repSm, '&lt;').replace(repGt, '&gt;')
+                        : value;
+                content = getFinalContent(formattedContent);
             }
-            return console.log(json);
-        };
-    };
+            htmlArray.push(`<li>${key}: ${content}</li>`);
+        }
+    }
+    htmlArray.push('</ul>');
+    return htmlArray.flat().join('');
+};
+
+function getFinalContent(formattedContent) {
+    if (formattedContent.length < 50 || typeof formattedContent == 'number') {
+        return `<span>${formattedContent}</span>`;
+    }
+    return `<span class="clickable" style="cursor: pointer">+</span><pre style="display:none">${formattedContent}</pre>`;
 }
 
-jsonToHTML(valueInput);
-
-function parseInput(input) {
+const parseInput = (input) => {
+    let json = {};
     try {
-        var json = typeof input === 'string' ? JSON.parse(input) : input;
+        json = typeof input === 'string' ? JSON.parse(input) : input;
     } catch (err) {
         console.error(err);
     }
     return json;
+};
+
+function setClickListeners() {
+    const clickableElements = document.getElementsByClassName('clickable');
+    Array.from(clickableElements).forEach((el) => {
+        el.onclick = () => {
+            const node = el.nextSibling;
+            if (node.style && node.style.display == 'none') {
+                node.style.display = 'block';
+                el.innerText = ' -';
+            } else if (node.style && node.style.display == 'block') {
+                node.style.display = 'none';
+                el.innerText = '+';
+            }
+        };
+    });
 }
 
-console.log(parseInput(valueInput));
+function buttonClickHandler() {
+    appendJSON(document.getElementById('text-area').value);
+}
+
+function appendJSON() {
+    const jsonHTML = jsonToHtml(valueInput);
+    fieldOutResult.innerHTML = jsonHTML;
+    setClickListeners();
+}
