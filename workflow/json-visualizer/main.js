7c0a08f2e737a1f1546adc97553a8e6f9dbe19da
Alex Gur <alexdream81@gmail.com>
2021-08-23 17:33:34 +0300

    fix: correct setClickListeners function

--- workflow/json-visualizer/main.js
+++ workflow/json-visualizer/main.js
@@ -25,33 +25,33 @@ fieldInput.value = `{
 const jsonToHtml = (data) => {
     return (show) => {
         const json = parseInput(data);
-        console.log(json);
-        const display = show ? 'block' : 'none';
         const htmlArray = [`<ul style="display: block">`];
+
         for (let [key, value] of Object.entries(json)) {
-            console.log(key);
             if (
                 typeof value === 'object' &&
                 !Array.isArray(value) &&
                 value !== null
             ) {
                 htmlArray.push(
-                    `<li class="clickable">${key} {${
+                    `<li  class="clickable">${key} {${
                         Object.keys(value).length
                     }}:`
                 );
                 htmlArray.push(jsonToHtml(value)(false));
             } else if (Array.isArray(value)) {
                 htmlArray.push(
-                    `<li class="clickable">${key} [${value.length}]:`
+                    `<li  class="clickable">${key} [${value.length}]:`
                 );
                 htmlArray.push(jsonToHtml(value)(false));
             } else {
-                let content;
-                content = value;
+                let content = value;
+
                 if (typeof content === 'number') {
                     htmlArray.push(
-                        `<li>${key}: <span class="text-color-red">${content}</span></li>`
+                        `<li>${key}: 
+                            <span class="text-color-red">${content}</span>
+                        </li>`
                     );
                 } else if (
                     typeof content === 'string' &&
@@ -59,19 +59,26 @@ const jsonToHtml = (data) => {
                     key !== 'boolean'
                 ) {
                     htmlArray.push(
-                        `<li>${key}: <span class="text-color-green">${content}</span></li>`
+                        `<li>${key}: 
+                            <span class="text-color-green">${content}</span>
+                        </li>`
                     );
                 } else if (key === 'boolean') {
                     htmlArray.push(
-                        `<li>${key}: <span class="text-color-orange">${content}</span></li>`
+                        `<li>${key}: 
+                            <span class="text-color-orange">${content}</span>
+                        </li>`
                     );
                 } else {
                     htmlArray.push(
-                        `<li>${key}: <span class="text-color-blue">${content}</span></li>`
+                        `<li>${key}: 
+                            <span class="text-color-blue">${content}</span>
+                        </li>`
                     );
                 }
             }
         }
+
         htmlArray.push('</ul>');
         return htmlArray.join('');
     };
@@ -79,6 +86,7 @@ const jsonToHtml = (data) => {
 
 const parseInput = (input) => {
     let json = {};
+
     try {
         json = typeof input === 'string' ? JSON.parse(input) : input;
     } catch (err) {
@@ -87,32 +95,24 @@ const parseInput = (input) => {
     return json;
 };
 
-// const setClickListeners = () => {
-//     const clickableElements = document.getElementsByClassName('clickable');
-//     Array.from(clickableElements).forEach((element) => {
-//         console.log(element.nextSibling);
-//         element.onclick = () => {
-//             const node = element.nextSibling;
-//             console.log(element.nextSibling);
-//             if (node.style && node.style.display === 'block') {
-//                 node.style.display = 'none';
-//             } else if (node.style && node.style.display === 'none') {
-//                 node.style.display = 'block';
-//             }
-//         };
-//     });
-// };
+const setClickListeners = () => {
+    const clickableElements = document.getElementsByClassName('clickable');
 
-// list.addEventListener('click', (event) => {
-//     if (event.target.getElementsByClassName === '.show-list') {
-//         ev.target.classList.toggle('done');
-//     }
-// });
+    Array.from(clickableElements).forEach((element) => {
+        element.onclick = () => {
+            const node = element.lastChild;
+
+            node.style?.display === 'block'
+                ? (node.style.display = 'none')
+                : (node.style.display = 'block');
+        };
+    });
+};
 
 button.addEventListener('click', () => appendJSON(fieldInput.value));
 
 const appendJSON = (valueInput) => {
     const jsonHTML = jsonToHtml(valueInput)(true);
     fieldOutResult.innerHTML = jsonHTML;
-    // setClickListeners();
+    setClickListeners();
 };
