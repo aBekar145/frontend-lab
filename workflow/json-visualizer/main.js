f5115f08589491e42ede98d5a6c4eb443a53305c
Alex Gur <alexdream81@gmail.com>
2021-08-23 10:22:43 +0300

    fix: correct style for value in a json file

--- workflow/json-visualizer/main.js
+++ workflow/json-visualizer/main.js
@@ -1,48 +1,80 @@
 const fieldInput = document.getElementById('text-area');
 const fieldOutResult = document.getElementById('show-here');
 const button = document.getElementById('text-area-button');
+const list = document.querySelector('ul');
+
+fieldInput.value = `{
+    "array": [
+      1,
+      2,
+      3,
+      4
+    ],
+    "boolean": "false",
+    "color": "gold",
+    "null": "null",
+    "number": 123,
+    "object": {
+      "a": "b",
+      "c": "d",
+      "e": "f"
+    },
+    "string": "Hello World"
+  }`;
 
 const jsonToHtml = (data) => {
     return (show) => {
         const json = parseInput(data);
+        console.log(json);
         const display = show ? 'block' : 'none';
-        const repSm = /</gi;
-        const repGt = />/gi;
-        const htmlArray = [`<ul style="display: ${display}">`];
+        const htmlArray = [`<ul style="display: block">`];
         for (let [key, value] of Object.entries(json)) {
-            if (typeof value === 'object' && Object.keys(value).length > 0) {
+            console.log(key);
+            if (
+                typeof value === 'object' &&
+                !Array.isArray(value) &&
+                value !== null
+            ) {
                 htmlArray.push(
-                    `<li>${key}:<span class="clickable" style="cursor: pointer"> ></span>`
+                    `<li class="clickable">${key} {${
+                        Object.keys(value).length
+                    }}:`
+                );
+                htmlArray.push(jsonToHtml(value)(false));
+            } else if (Array.isArray(value)) {
+                htmlArray.push(
+                    `<li class="clickable">${key} [${value.length}]:`
                 );
                 htmlArray.push(jsonToHtml(value)(false));
             } else {
                 let content;
-                if (Array.isArray(value)) {
-                    content = '[]';
-                } else if (typeof value === 'object') {
-                    content = '{}';
+                content = value;
+                if (typeof content === 'number') {
+                    htmlArray.push(
+                        `<li>${key}: <span class="text-color-red">${content}</span></li>`
+                    );
+                } else if (
+                    typeof content === 'string' &&
+                    content !== 'null' &&
+                    key !== 'boolean'
+                ) {
+                    htmlArray.push(
+                        `<li>${key}: <span class="text-color-green">${content}</span></li>`
+                    );
+                } else if (key === 'boolean') {
+                    htmlArray.push(
+                        `<li>${key}: <span class="text-color-orange">${content}</span></li>`
+                    );
                 } else {
-                    const formattedContent =
-                        typeof value === 'string'
-                            ? value
-                                  .replace(repSm, '&lt;')
-                                  .replace(repGt, '&gt;')
-                            : value;
-                    content = getFinalContent(formattedContent);
+                    htmlArray.push(
+                        `<li>${key}: <span class="text-color-blue">${content}</span></li>`
+                    );
                 }
-                htmlArray.push(`<li>${key}: ${content}</li>`);
             }
         }
         htmlArray.push('</ul>');
         return htmlArray.join('');
     };
-};
-
-const getFinalContent = (formattedContent) => {
-    if (formattedContent.length < 50 || typeof formattedContent == 'number') {
-        return `<span>${formattedContent}</span>`;
-    }
-    return `<span class="clickable" style="cursor: pointer">></span><pre style="display:block">${formattedContent}</pre>`;
 };
 
 const parseInput = (input) => {
@@ -55,26 +87,32 @@ const parseInput = (input) => {
     return json;
 };
 
-const setClickListeners = () => {
-    const clickableElements = document.getElementsByClassName('clickable');
-    Array.from(clickableElements).forEach((element) => {
-        element.onclick = () => {
-            const node = element.nextSibling;
-            if (node.style && node.style.display == 'none') {
-                node.style.display = 'block';
-                element.innerText = ' v';
-            } else if (node.style && node.style.display == 'block') {
-                node.style.display = 'none';
-                element.innerText = ' >';
-            }
-        };
-    });
-};
+// const setClickListeners = () => {
+//     const clickableElements = document.getElementsByClassName('clickable');
+//     Array.from(clickableElements).forEach((element) => {
+//         console.log(element.nextSibling);
+//         element.onclick = () => {
+//             const node = element.nextSibling;
+//             console.log(element.nextSibling);
+//             if (node.style && node.style.display === 'block') {
+//                 node.style.display = 'none';
+//             } else if (node.style && node.style.display === 'none') {
+//                 node.style.display = 'block';
+//             }
+//         };
+//     });
+// };
+
+// list.addEventListener('click', (event) => {
+//     if (event.target.getElementsByClassName === '.show-list') {
+//         ev.target.classList.toggle('done');
+//     }
+// });
 
 button.addEventListener('click', () => appendJSON(fieldInput.value));
 
 const appendJSON = (valueInput) => {
     const jsonHTML = jsonToHtml(valueInput)(true);
     fieldOutResult.innerHTML = jsonHTML;
-    setClickListeners();
+    // setClickListeners();
 };
