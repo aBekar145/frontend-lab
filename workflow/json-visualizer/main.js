c481561d4b6103481a62a3cc0b0226f8e528131d
Alex Gur <alexdream81@gmail.com>
2021-08-23 18:47:48 +0300

    fix: remove return in jsonToHtml function

--- workflow/json-visualizer/main.js
+++ workflow/json-visualizer/main.js
@@ -3,65 +3,59 @@ const fieldOutResult = document.getElementById('show-here');
 const button = document.getElementById('text-area-button');
 
 const jsonToHtml = (data) => {
-    return (show) => {
-        const json = parseInput(data);
-        const htmlArray = [`<ul style="display: block">`];
+    const json = parseInput(data);
+    const htmlArray = [`<ul style="display: block">`];
 
-        for (let [key, value] of Object.entries(json)) {
-            if (
-                typeof value === 'object' &&
-                !Array.isArray(value) &&
-                value !== null
+    for (let [key, value] of Object.entries(json)) {
+        if (
+            typeof value === 'object' &&
+            !Array.isArray(value) &&
+            value !== null
+        ) {
+            htmlArray.push(
+                `<li  class="clickable">${key} {${Object.keys(value).length}}:`
+            );
+            htmlArray.push(jsonToHtml(value));
+        } else if (Array.isArray(value)) {
+            htmlArray.push(`<li  class="clickable">${key} [${value.length}]:`);
+            htmlArray.push(jsonToHtml(value));
+        } else {
+            let content = value;
+
+            if (typeof content === 'number') {
+                htmlArray.push(
+                    `<li>${key}: 
+                        <span class="text-color-red">${content}</span>
+                    </li>`
+                );
+            } else if (
+                typeof content === 'string' &&
+                content !== 'null' &&
+                key !== 'boolean'
             ) {
                 htmlArray.push(
-                    `<li  class="clickable">${key} {${
-                        Object.keys(value).length
-                    }}:`
+                    `<li>${key}: 
+                        <span class="text-color-green">${content}</span>
+                    </li>`
                 );
-                htmlArray.push(jsonToHtml(value)(false));
-            } else if (Array.isArray(value)) {
+            } else if (key === 'boolean') {
                 htmlArray.push(
-                    `<li  class="clickable">${key} [${value.length}]:`
+                    `<li>${key}: 
+                        <span class="text-color-orange">${content}</span>
+                    </li>`
                 );
-                htmlArray.push(jsonToHtml(value)(false));
             } else {
-                let content = value;
-
-                if (typeof content === 'number') {
-                    htmlArray.push(
-                        `<li>${key}: 
-                            <span class="text-color-red">${content}</span>
-                        </li>`
-                    );
-                } else if (
-                    typeof content === 'string' &&
-                    content !== 'null' &&
-                    key !== 'boolean'
-                ) {
-                    htmlArray.push(
-                        `<li>${key}: 
-                            <span class="text-color-green">${content}</span>
-                        </li>`
-                    );
-                } else if (key === 'boolean') {
-                    htmlArray.push(
-                        `<li>${key}: 
-                            <span class="text-color-orange">${content}</span>
-                        </li>`
-                    );
-                } else {
-                    htmlArray.push(
-                        `<li>${key}: 
-                            <span class="text-color-blue">${content}</span>
-                        </li>`
-                    );
-                }
+                htmlArray.push(
+                    `<li>${key}: 
+                        <span class="text-color-blue">${content}</span>
+                    </li>`
+                );
             }
         }
+    }
 
-        htmlArray.push('</ul>');
-        return htmlArray.join('');
-    };
+    htmlArray.push('</ul>');
+    return htmlArray.join('');
 };
 
 const parseInput = (input) => {
@@ -70,7 +64,7 @@ const parseInput = (input) => {
     try {
         json = typeof input === 'string' ? JSON.parse(input) : input;
     } catch (err) {
-        console.error(err);
+        alert(err);
     }
     return json;
 };
@@ -96,7 +90,7 @@ const setClickListeners = () => {
 button.addEventListener('click', () => appendJSON(fieldInput.value));
 
 const appendJSON = (valueInput) => {
-    const jsonHTML = jsonToHtml(valueInput)(true);
+    const jsonHTML = jsonToHtml(valueInput);
     fieldOutResult.innerHTML = jsonHTML;
 
     setClickListeners();
