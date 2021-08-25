ffd0b20272568dfcaa95750274420239b495f7c9
Alex Gur <alexdream81@gmail.com>
2021-08-25 18:18:58 +0300

    feat: add getClassBy function

--- workflow/json-visualizer/main.js
+++ workflow/json-visualizer/main.js
@@ -2,55 +2,40 @@ const fieldInput = document.getElementById('text-area');
 const fieldOutResult = document.getElementById('show-here');
 const button = document.getElementById('text-area-button');
 
+const getClassBy = (value) => {
+    const typeValue = typeof value;
+    const key = value === null ? value : typeValue;
+    const dataObject = {
+        number: 'text-color-red',
+        boolean: 'text-color-orange',
+        string: 'text-color-green',
+        null: 'text-color-blue',
+        default: 'text-color-black',
+    };
+
+    return dataObject[key] || dataObject.default;
+};
+
 const jsonToHtml = (data) => {
     const json = parseInput(data);
     const htmlArray = [`<ul style="display: block">`];
 
     for (let [key, value] of Object.entries(json)) {
-        if (
-            typeof value === 'object' &&
-            !Array.isArray(value) &&
-            value !== null
-        ) {
-            htmlArray.push(
-                `<li  class="clickable">${key} {${Object.keys(value).length}}:`
-            );
-            htmlArray.push(jsonToHtml(value));
-        } else if (Array.isArray(value)) {
-            htmlArray.push(`<li  class="clickable">${key} [${value.length}]:`);
+        if (typeof value === 'object' && value !== null) {
+            const lengthObject =
+                typeof value === 'object' && !Array.isArray(value)
+                    ? `{${Object.keys(value).length}}`
+                    : `[${value.length}]`;
+            htmlArray.push(`<li class="clickable">${key} ${lengthObject}:`);
             htmlArray.push(jsonToHtml(value));
         } else {
             let content = value;
-
-            if (typeof content === 'number') {
-                htmlArray.push(
-                    `<li>${key}: 
-                        <span class="text-color-red">${content}</span>
-                    </li>`
-                );
-            } else if (
-                typeof content === 'string' &&
-                content !== 'null' &&
-                key !== 'boolean'
-            ) {
-                htmlArray.push(
-                    `<li>${key}: 
-                        <span class="text-color-green">${content}</span>
-                    </li>`
-                );
-            } else if (key === 'boolean') {
-                htmlArray.push(
-                    `<li>${key}: 
-                        <span class="text-color-orange">${content}</span>
-                    </li>`
-                );
-            } else {
-                htmlArray.push(
-                    `<li>${key}: 
-                        <span class="text-color-blue">${content}</span>
-                    </li>`
-                );
-            }
+            const styleClass = getClassBy(content);
+            htmlArray.push(
+                `<li>${key}: 
+                    <span class="${styleClass}">${content}</span>
+                </li>`
+            );
         }
     }
 
@@ -66,6 +51,7 @@ const parseInput = (input) => {
     } catch (err) {
         alert(err);
     }
+
     return json;
 };
 
@@ -87,11 +73,11 @@ const setClickListeners = () => {
     });
 };
 
-button.addEventListener('click', () => appendJSON(fieldInput.value));
-
 const appendJSON = (valueInput) => {
     const jsonHTML = jsonToHtml(valueInput);
     fieldOutResult.innerHTML = jsonHTML;
 
     setClickListeners();
 };
+
+button.addEventListener('click', () => appendJSON(fieldInput.value));
