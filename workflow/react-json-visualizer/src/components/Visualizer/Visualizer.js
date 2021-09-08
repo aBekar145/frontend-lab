44006b1382e5cf44a0b4e309a5a363ea7a3cfb29
Александр <aBekar145@gmail.com>
2021-09-08 19:43:06 +0300

    feat: add styles in project

--- workflow/react-json-visualizer/src/components/Visualizer/Visualizer.js
+++ workflow/react-json-visualizer/src/components/Visualizer/Visualizer.js
@@ -1,19 +1,18 @@
 import React from "react";
 import classes from './Visualizer.module.scss'
 
-
 const Visualizer = (props) => {
   return (
     <div className={classes.wrap}>
         <div className={classes.sizeBlock}>
-            <label className="text-area-subtitle subtitle" for="text-area">Add JSON files:</label>
-            <textarea id="text-area" className="textarea-input-field"></textarea>
+            <label className={classes.subtitle} for="text-area">Add JSON files:</label>
+            <textarea id="text-area" className={classes.textareaInputField}></textarea>
         </div>
 
-        <button className="button" id="text-area-button">Build tree data</button>
+        <button className={classes.button} id="text-area-button">Build tree data</button>
 
         <div className={classes.sizeBlock}>
-            <h3 className="subtitle">Output of finished data:</h3>
+            <h3 className={classes.subtitle}>Output of finished data:</h3>
             <div className={classes.outputField} id="show-here"></div>
          </div>
     </div>
