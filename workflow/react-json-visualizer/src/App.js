5d817e85245860c0f71e740cfc2140ae234de070
Александр <aBekar145@gmail.com>
2021-09-09 22:43:46 +0300

    feat: add handleClick and handleInput functions

--- workflow/react-json-visualizer/src/App.js
+++ workflow/react-json-visualizer/src/App.js
@@ -1,25 +1,16 @@
 import React, {Component} from 'react';
 import Visualizer from './components/Visualizer/Visualizer'
 import classes from './App.module.scss'
+import { times } from 'lodash';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pageTitle: 'JSON Editor',
-      fieldInputSubtitle: 'Add JSON files:',
-      fieldShowSubtitle: 'Output of finished data:',
-      submitButton: 'Build tree data'
-    }
-  }
+
   render() {
     return (
       <div className={classes.block}>
-        <h1 className={classes.title}>{this.state.pageTitle}</h1>
+        <h1 className={classes.title}>Hello</h1>
         <Visualizer
-          fieldInputSubtitle={this.state.fieldInputSubtitle}
-          fieldShowSubtitle={this.state.fieldShowSubtitle}
-          submitButton={this.state.submitButton}
+         state={this.state}
         />
       </div>
     )
--- workflow/react-json-visualizer/src/components/Visualizer/Visualizer.js
+++ workflow/react-json-visualizer/src/components/Visualizer/Visualizer.js
@@ -2,24 +2,44 @@ import React from "react";
 import classes from './Visualizer.module.scss'
 
 class Visualizer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageTitle: 'JSON Editor',
+      fieldInputSubtitle: 'Add JSON files:',
+      fieldShowSubtitle: 'Output of finished data:',
+      submitButton: 'Build tree data',
+      outResult: null,
+      input: null
+    }
+  }
 
   handleInput = (event) => {
-    this.setState({pageTitle: event.target.value})
+    let body = event.target.value
+    this.input = body;
   }
+
+  handleClick = () => {
+    let out = this.input;
+    this.setState({
+      outResult: out
+    })
+  }
+
   render() {
 
     return (
       <div className={classes.wrap}>
           <div className={classes.sizeBlock}>
-              <label className={classes.subtitle} htmlFor ="text-area">{this.props.fieldInputSubtitle}</label>
+              <label className={classes.subtitle} htmlFor ="text-area">{this.state.fieldInputSubtitle}</label>
               <textarea onChange={this.handleInput} id="text-area" className={classes.textareaInputField}></textarea>
           </div>
 
-          <button className={classes.button} id="text-area-button">{this.props.submitButton}</button>
+          <button onClick={this.handleClick.bind(this)} className={classes.button} id="text-area-button">{this.state.submitButton}</button>
 
           <div className={classes.sizeBlock}>
-              <h3 className={classes.subtitle}>{this.props.fieldShowSubtitle}</h3>
-              <div className={classes.outputField} id="show-here"></div>
+              <h3 className={classes.subtitle}>{this.state.fieldShowSubtitle}</h3>
+              <div className={classes.outputField} id="show-here">{this.state.outResult}</div>
            </div>
       </div>
     )
