7211c8f0198e38464bcfcff94eea02b1545ee9be
Alex Gur <alexdream81@gmail.com>
2021-10-10 12:38:29 +0300

    init

---
+++ workflow/react-project/src/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CurrentTime from '../components/CurrentTime'
+
+const App = () => {
+    return (
+        <div>
+            <CurrentTime />
+        </div>
+    )
+}
+
+export default App;
---
+++ workflow/react-project/src/components/CurrentTime/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CurrentTime = () => {
+    return <h1>Hello world</h1>
+}
+
+export default CurrentTime;
