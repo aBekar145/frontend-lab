f84402338ee2394cf19676eb193df3963548bbf5
Aliaksandr Hur <Aliaksandr.Hur@itechart-group.com>
2021-10-11 18:54:53 +0300

    feat: add animation for text

--- workflow/react-project-page/src/components/Main/ContentBlock/Description/Description.js
+++ workflow/react-project-page/src/components/Main/ContentBlock/Description/Description.js
@@ -4,13 +4,20 @@ import classes from './Description.module.scss';
 
 const Description = () => {
   return (
-    <div className={classes.wrap}>
-      <p className={classes.text}>
-        Everybody should believe in something I believe I’ll have another drink.
-      </p>
-      <p className={classes.text}>
-        “I’m on a whisky diet. I’ve lost three days already.” — Tommy Cooper
-      </p>
+    <div className={classes.container}>
+      <div className={classes.wrap + ' ' + classes.flip}>
+        <div className={classes.text}>
+          Everybody should believe in something I believe I’ll have another
+          drink.
+        </div>
+        <div className={classes.text}>
+          “I’m on a whisky diet. I’ve lost three days already.” — Tommy Cooper
+        </div>
+        <div className={classes.text}>
+          A cocktail is an alcoholic mixed drink.A cocktail is an alcoholic
+          mixed drink
+        </div>
+      </div>
     </div>
   );
 };
