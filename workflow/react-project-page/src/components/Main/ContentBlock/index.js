8253d2ca7ce12194c7bc03151f5dc1ef7f5030b7
Aliaksandr Hur <Aliaksandr.Hur@itechart-group.com>
2021-10-13 10:07:57 +0300

    fix: correct adaptive in the project

--- workflow/react-project-page/src/components/Main/ContentBlock/index.js
+++ workflow/react-project-page/src/components/Main/ContentBlock/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Description from './Description/Description';
 import Image from './Image';
 
-import classes from './ConstentBlock.module';
+import classes from './ContentBlock.module';
 
 const ContentBlock = ({ handleButton }) => {
   return (
