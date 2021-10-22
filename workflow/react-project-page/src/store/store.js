c0d99ff08cb71668f4ee4aeee93f5b48d19290c0
Alex Gur <alexdream81@gmail.com>
2021-10-22 14:24:33 +0300

    fix: correct packets

--- workflow/react-project-page/src/store/store.js
+++ workflow/react-project-page/src/store/store.js
@@ -1,16 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/rootReducer';
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
 const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
